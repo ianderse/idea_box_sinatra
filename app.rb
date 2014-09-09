cf9bbcae599c981839d10c21639a7c7b53749733
Ian Andersen <ianderse@mac.com>
2014-09-08 18:37:18 -0600

    complete I1

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'bundler'
+require_relative 'idea'
+
+Bundler.require
+
+class IdeaBoxApp < Sinatra::Base
+	configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+  	idea = Idea.new
+  	idea.save
+  	"Creating an IDEA!"
+	end
+
+end
