7e68d1583a5f2ad43f558518e1dbf9f70cb0ea47
Ian Andersen <ianderse@mac.com>
2014-09-09 14:14:23 -0600

    finish I9

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -1,10 +1,34 @@
 class Idea
+	include Comparable
 
-	attr_reader :title, :description
+	attr_reader :title, :description, :rank, :id
 
-	def initialize(attributes)
-	  @title = attributes["title"]
+	def initialize(attributes = {})
+	  @title 			 = attributes["title"]
 	  @description = attributes["description"]
+	  @rank 		   = attributes["rank"] || 0
+	  @id 				 = attributes["id"]
+	end
+
+	def save
+		IdeaStore.create(to_h)
+	end
+
+	def to_h
+		{
+			"title" => title,
+			"description" => description,
+			"rank" => rank,
+			"id" => id
+		}
+	end
+
+	def like!
+		@rank += 1
+	end
+
+	def <=>(other)
+		other.rank <=> rank
 	end
 
 end
--- lib/idea_box/idea_store.rb
+++ lib/idea_box/idea_store.rb
@@ -4,10 +4,9 @@ require_relative 'idea'
 
 class IdeaStore
 
-	def self.create(attributes)
+	def self.create(data)
 		database.transaction do
-			database['ideas'] ||= []
-			database['ideas'] << attributes
+			database['ideas'] << data
 		end
 	end
 
@@ -18,7 +17,8 @@ class IdeaStore
 	end
 
 	def self.find(id)
-		Idea.new(find_raw_idea(id))
+		raw_idea = find_raw_idea(id)
+		Idea.new(raw_idea.merge("id" => id))
 	end
 
 	def self.find_raw_idea(id)
@@ -28,9 +28,11 @@ class IdeaStore
 	end
 
 	def self.all
-	  raw_ideas.map do |data|
-	    Idea.new(data)
+		ideas = []
+	  raw_ideas.each_with_index do |data, i|
+	    ideas << Idea.new(data.merge("id" => i))
 	  end
+	  ideas
 	end
 
 	def self.raw_ideas
@@ -46,6 +48,12 @@ class IdeaStore
 	end
 
 	def self.database
-    @database ||= YAML::Store.new("db/ideabox")
+		return @database if @database
+
+    @database = YAML::Store.new("db/ideabox")
+    @database.transaction do
+    	@database['ideas'] ||= []
+    end
+    @database
   end
 end
